767306752287b0feac16329b140d9ea4bb2a6186
Jack Valmadre <jack.valmadre@gmail.com>
2014-08-01 16:06:28 -0400

    Add ReadLines

---
+++ fileutil/lines.go
@@ -0,0 +1,25 @@
+package fileutil
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadLines(fname string) ([]string, error) {
+	file, err := os.Open(fname)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	// Read through lines of file.
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
